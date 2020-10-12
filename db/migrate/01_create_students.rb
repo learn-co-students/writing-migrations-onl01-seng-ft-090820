9d957c8dd9dd70085e32e9666291e9ac24fcbc3a
J K <jkim65@buffalo.edu>
2020-10-12 23:09:42 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,11 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+
+  def change
+    create_table :students do |s|
+      s.string :name
+    end
+  end
 end
+# create a table with Active Record.
+# define method called change
+# run migrations rake db:migrate
